3237a83c36b62a9596c9f5444bea97dcd3eefaae
Ari <ar-to@users.noreply.github.com>
2017-07-01 16:54:44 -0700

    Feature/less command (#1)
    * created new command for less; reading file utily working per command; cleaned up modules;
    
    * updated app.js to contain vorpal commands; began working on callback logic; began prep for watch and config.json features;
    
    * updatedand began testing function that will run all logic
    
    * Release v.0.1.0

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 #!/usr/bin/env node
 const path = require('path'),
   fs = require('fs'),
-  cmds = require('./lib/vorpal'),
-  dir = require('./lib/directories'),
-  file = require('./lib/new_file');
-  //command = require('./lib/commands');
+  vorpal = require('vorpal')(),
+  readLessFile = require('./lib/read_file'),
+  lessCompile = require('./lib/compile_less').compile,
+  createFile = require('./lib/create_file').createFile;
 
+console.log('Please choose a fileName with extension [.less] to compile. Use help command for possible commands.');
 
+vorpal
+  .delimiter('lcc$')
+  .show();
 
-console.log(cmds.test, file.test, dir.test);
-console.log(process.cwd());
-console.log(process.cwd(__filename) + '/*.less');
+vorpal
+  .command('less [filename] [sourceDir] [cssFileName]', 'Choose directory & filename.less to compile. Detaults to cwd')
+  .option('-d --default', 'Uses default values [lessMain, ./ , cssMain]')
+  .action(function(args, cb) {
+    if (args.filename) {
+      readAndCompileToCss(args.filename, args.sourceDir, args.cssFileName);
+    }
+    if (args.options.default) {
+      readAndCompileToCss(args.filename, args.sourceDir, args.cssFileName);
+    }
+
+    cb();
+  })
+
+function readAndCompileToCss(lessFileLess, sourceDir, cssFileName) {
+    readLessFile(lessFileLess, sourceDir, function (err, dataString) {
+      if (err) return console.log(err.message);
+        //console.log(`This is your file content: ${dataString}`);
+
+      lessCompile(dataString, function(err, cssData) {
+          //console.log('this is the css data as string:' + ' ' + cssData.toString());
+        createFile(cssFileName, cssData);
+      });//end lessCompile
+    });//end readLessFile
+}//end readAndCompileToCss
